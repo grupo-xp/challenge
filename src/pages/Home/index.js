c198f3fe50de109d37ac7cb853121e8ae10486bc
Prince Vasconcelos <joaquim.vasconcelos@confitec.com.br>
2021-03-05 21:54:33 -0300

    create Pages Album and Home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'components/Link'
+
+export default function Home () {
+    return (
+        <div>
+            Ola prince
+            <Link to="/albums/mc-kevinho">ALbum</Link>
+        </div>
+    )
+}
