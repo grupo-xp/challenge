4e0db955cdcde8f961029e40b3ca8ed276abbcd7
Prince Vasconcelos <joaquim.vasconcelos@confitec.com.br>
2021-03-09 17:27:58 -0300

    create Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,50 @@
-import React from 'react'
-import Link from 'components/Link'
+import React, {useState, useMemo} from 'react'
+
+import API from 'service'
+
+import Search from 'components/Search'
+import Gallery from 'components/Gallery'
 
 export default function Home () {
+    const [searchValue, setSearchValue] = useState('')
+    const [searchResult, setSearchResult] = useState({
+        items: [],
+        word: ''
+    })
+    const [timeoutId, setTimeoutId] = useState(null)
+    const diacriticsRegex = /[\u0300-\u036f]/g
+    const spaceRegex = /\s/
+
+    const handleSearchType = ({ target: { value } }) => {
+        setSearchValue(value)
+
+        if (timeoutId) clearTimeout(timeoutId)
+
+        setTimeoutId(setTimeout(
+            () => searchAlbumOrArtistOnSpotifyAPI(value), 1000)
+        )
+    }
+
+    const searchAlbumOrArtistOnSpotifyAPI = async word => {
+        if (!word) return
+
+        const sanitizedWord = word
+            .normalize('NFD')
+            .replace(diacriticsRegex, '')
+            .replace(spaceRegex, '%20')
+
+        return await API.get(
+            `https://api.spotify.com/v1/search?q=${sanitizedWord}&type=album,track,artist`
+        ).then(({ albums, tracks, artists }) => {
+            setSearchResult({ items: albums.items, word })
+        })
+        // salvar esses dados num local global
+    }
+
     return (
         <div>
-            Ola prince
-            <Link to="/albums/mc-kevinho">ALbum</Link>
+            <Search value={searchValue} onChange={handleSearchType} />
+            <Gallery data={searchResult.items} word={searchResult.word} />
         </div>
     )
 }
