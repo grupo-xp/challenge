50139b846e567972f210e31a8907302fd475e4e9
Prince Vasconcelos <joaquim.vasconcelos@confitec.com.br>
2021-03-09 17:27:51 -0300

    create Album page

--- src/pages/Album/index.js
+++ src/pages/Album/index.js
@@ -1,7 +1,43 @@
 import React from 'react'
+import styled from 'styled-components'
+import { useHistory } from 'react-router-dom'
+import { ReactComponent as BackIcon } from 'assets/back.svg';
 
-export default function Home () {
+import Link from 'components/Link'
+
+const BackContainer = styled(Link)`
+  display: flex;
+  justify-content: center;
+
+  span {
+      background: crimson;
+      color: white;
+  }
+`
+
+const Text = styled.span`
+  color: white;
+`
+
+const Container = styled.div`
+    display: flex;
+`
+
+export default function Album () {
+    const history = useHistory()
+
+    const handleBack = () => history.goBack()
     return (
-        <div>Album</div>
+        <Container id="princelokao">
+            <BackIcon 
+                    style={{ cursor: 'pointer' }}
+                    onClick={handleBack}
+                    fill="white"
+                    width="16"
+                    height="16"
+                />
+            <Text>Voltar</Text>
+            
+        </Container>
     )
 }
